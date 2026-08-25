6123ef088210865d4d87838cfd96650333c9dcf0
arkadacasinos <bezdep.rf@mail.ru>
2026-08-25 20:17:58 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -102,6 +102,7 @@ export default function RootLayout({
   return (
     <html lang="ru" dir="ltr" className={`${evaSans.variable} ${evaSerif.variable}`}>
       <head>
+        <meta name="yandex-verification" content="00d30b6be7fe2c58" />
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <link rel="canonical" href="https://evacasino89.vercel.app/" />
